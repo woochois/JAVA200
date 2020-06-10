9c8f284f228525bbfe7a837ac470b26b6e84441c
Hotgeorge <65319336+Hotgeorge@users.noreply.github.com>
2020-06-10 17:45:20 +0900

    자바 200제 책 환경설정

---
+++ javabookfx/src/kr/co/infopub/BillboardController.java
@@ -0,0 +1,17 @@
+package kr.co.infopub;
+
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+
+public class BillboardController {
+
+    @FXML
+    private Button btnHello;
+
+    @FXML
+    void onBtnHelloAction(ActionEvent event) {
+    	
+    }
+
+}
